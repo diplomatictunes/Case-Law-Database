59d7d0e9f2007fd090201c2568167d67a6878b0d
diplomatictunes <admin@diplomatictunes.com>
2024-04-15 06:36:25 +1000

    remove tailscale from homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 function HomePage() {
     return (
-        <div className="flex myblue justify-center items-center h-screen">
-            <div className="w-full max-w-md bg-gray-300 shadow-lg rounded overflow-hidden p-8">
-                <h1 className="text-3xl font-bold mb-4 text-center">DataCase</h1>
-                <div className="flex justify-between">
+        <div>
+            <div>
+                <h1>DataCase</h1>
+                <div>
                     <Link to="/search-results">
-                        <button className="bg-mygreen hover:bg-teal-500 focus:shadow-outline focus:outline-none text-mycream py-2 px-4 rounded mr-2">
+                        <button>
                             Search Cases
                         </button>
                     </Link>
                     <Link to="/submit-case">
-                        <button className="bg-mygreen hover:bg-teal-500 focus:shadow-outline focus:outline-none text-mycream py-2 px-4 rounded">
+                        <button>
                             Submit New Case
                         </button>
                     </Link>
